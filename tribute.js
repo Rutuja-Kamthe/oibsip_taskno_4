08ee0068b248588a24275a3584abdfd6a7e4bd45
Rutuja-Kamthe <90389568+Rutuja-Kamthe@users.noreply.github.com>
2022-02-01 09:18:28 +0530

    Add files via upload

---
+++ tribute.js
@@ -0,0 +1,13 @@
+/**
+ * @description      : 
+ * @author           : HP
+ * @group            : 
+ * @created          : 31/01/2022 - 19:35:16
+ * 
+ * MODIFICATION LOG
+ * - Version         : 1.0.0
+ * - Date            : 31/01/2022
+ * - Author          : HP
+ * - Modification    : 
+ **/
+const projectName = 'tribute-page';
